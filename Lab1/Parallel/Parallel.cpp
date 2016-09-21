e84b0aee1df0a77c77f1bbf850dfc12ea6cec83a
7kia <il7kia@yandex.ru>
2016-09-21 15:22:55 +0300

    Delete excess code

--- Lab1/Parallel/Parallel.cpp
+++ Lab1/Parallel/Parallel.cpp
@@ -3,217 +3,38 @@
 
 #include "stdafx.h"
 #include "Parallel.h"
+#include "../MatrixOperations.h"
+#include <array>
 
 #include <string>
-/*
 
-
-#define MAX_LOADSTRING 100
-
-// Глобальные переменные:
-HINSTANCE hInst;                                // текущий экземпляр
-WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
-WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
-
-// Отправить объявления функций, включенных в этот модуль кода:
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
-
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
-{
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
-
-    // TODO: разместите код здесь.
-
-    // Инициализация глобальных строк
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_PARALLEL, szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance);
-
-    // Выполнить инициализацию приложения:
-    if (!InitInstance (hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
-
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_PARALLEL));
-
-    MSG msg;
-
-    // Цикл основного сообщения:
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-    }
-
-    return (int) msg.wParam;
-}
-
-
-
-//
-//  ФУНКЦИЯ: MyRegisterClass()
-//
-//  НАЗНАЧЕНИЕ: регистрирует класс окна.
-//
-ATOM MyRegisterClass(HINSTANCE hInstance)
-{
-    WNDCLASSEXW wcex;
-
-    wcex.cbSize = sizeof(WNDCLASSEX);
-
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_PARALLEL));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_PARALLEL);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
-
-    return RegisterClassExW(&wcex);
-}
-
-//
-//   ФУНКЦИЯ: InitInstance(HINSTANCE, int)
-//
-//   НАЗНАЧЕНИЕ: сохраняет обработку экземпляра и создает главное окно.
-//
-//   КОММЕНТАРИИ:
-//
-//        В данной функции дескриптор экземпляра сохраняется в глобальной переменной, а также
-//        создается и выводится на экран главное окно программы.
-//
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
-{
-   hInst = hInstance; // Сохранить дескриптор экземпляра в глобальной переменной
-
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
-
-   if (!hWnd)
-   {
-      return FALSE;
-   }
-
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
-
-   return TRUE;
-}
-
-//
-//  ФУНКЦИЯ: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  НАЗНАЧЕНИЕ:  обрабатывает сообщения в главном окне.
-//
-//  WM_COMMAND — обработать меню приложения
-//  WM_PAINT — отрисовать главное окно
-//  WM_DESTROY — отправить сообщение о выходе и вернуться
-//
-//
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    switch (message)
-    {
-    case WM_COMMAND:
-        {
-            int wmId = LOWORD(wParam);
-            // Разобрать выбор в меню:
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
-        }
-        break;
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: Добавьте сюда любой код прорисовки, использующий HDC...
-            EndPaint(hWnd, &ps);
-        }
-        break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
-    return 0;
-}
-
-// Обработчик сообщений для окна "О программе".
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
-
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
-}
-*/
 #include <stdio.h>
 #include <Windows.h>
 #include <time.h>
 
-#define MATRIX_SIZE 4
+// For timer
+#include <boost/chrono.hpp>
+#include <boost/timer/timer.hpp>
+#include <boost/algorithm/string.hpp>
 
-struct row
+static const int MATRIX_SIZE = 500;
+static const int AMOUNT_THREAD = 4;
+static const size_t SIZE_SECTION = INPUT_MATRIX.size() / AMOUNT_THREAD * size_t(2);
+static const int ROUNDING_NUMBER = 5;
+
+struct DataForProgram
 {
-	int value[MATRIX_SIZE];
-	int result;
-	int rnd;
+	Matrix input;
+	Matrix output;
 };
 
 
-DWORD WINAPI generateAndCalc(void *data)
+DWORD WINAPI ComputeMinorsMatrix(void *data)
 {
 	//преобразуем полученные данные к типу структуры
-	row *r = (row *)data;
-	//инициализируем генератор случайных чисел полученным числом
-	srand(r->rnd);
-	//генерируем элементы строки
-	for (int i = 0; i < MATRIX_SIZE; i++)
-	{
-		r->value[i] = rand() % 10;
-	}
-	//находим произведение нечетных элементов
-	r->result = 1;
-	for (int i = 0; i < MATRIX_SIZE; i += 2)
-	{
-		r->result *= r->value[i];
-	}
+	DataForProgram *r = (DataForProgram *)data;
+	
+
 	return 0;
 }
 
@@ -241,30 +62,73 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	//идентификаторы потоков и структуры для строк матрицы
 	HANDLE thread[MATRIX_SIZE];
 	DWORD thrId[MATRIX_SIZE];
-	row rows[MATRIX_SIZE];
 
-	for (int i = 0; i < MATRIX_SIZE; i++)
+	DataForProgram sourcesData;
+	sourcesData.input = INPUT_MATRIX;
+	sourcesData.output.resize(INPUT_MATRIX.size(), MatrixRow(INPUT_MATRIX.size()));
+
+	boost::timer::cpu_timer timer;
+
+	//
+	// Start
+	// 
+	timer.start();
+	
+
+	///*
+	std::array<DataForProgram, AMOUNT_THREAD> dataForThread;
+	for (int i = 0; i < AMOUNT_THREAD; i++)
 	{
-		//генерируем случайные числа для каждой строки
-		rows[i].rnd = rand();
 		//создаем потоки
-		thread[i] = CreateThread(NULL, 0, &generateAndCalc, &rows[i], 0, &thrId[i]);
+		Vector2UL startMatrix = Vector2UL((i % INPUT_MATRIX.size()) * SIZE_SECTION, (i / INPUT_MATRIX.size()) * SIZE_SECTION);
+		thread[i] = CreateThread(NULL, 0, &ComputeMinorsMatrix, &sourcesData, 0, &thrId[i]);
 	}
 	//ждем, пока все эти потоки завершатся
 	WaitForMultipleObjects(MATRIX_SIZE, thread, TRUE, INFINITE);
 
 	std::string input;
+
 	//выводим результат работы программы на экран
 	for (int i = 0; i < MATRIX_SIZE; i++)
 	{
 		for (int j = 0; j < MATRIX_SIZE; j++)
 		{
-			input += " " + std::to_string(rows[i].value[j]);
+			//input += " " + std::to_string(rows[i].value[j]);
 			//printf(" %d", rows[i].value[j]);
 		}
 		//printf(" | multipl = %d\n", rows[i].result);
-		input += " | multipl = " + std::to_string(rows[i].result) + "\n";
+		//input += " | multipl = " + std::to_string(rows[i].result) + "\n";
 	}
+	//*/
+
+	/*
+		// OLD main
+	char * ch = "A\0";
+	char * ch2 = "B\0";
+
+	std::string input;
+
+	HANDLE * handles = new HANDLE[2];
+	handles[0] = CreateThread(NULL, 0, &ThreadProc, ch, CREATE_SUSPENDED, NULL);
+	handles[1] = CreateThread(NULL, 0, &ThreadProc, ch2, CREATE_SUSPENDED, NULL);
+
+	ResumeThread(handles[0]);
+	ResumeThread(handles[1]);
+
+	WaitForMultipleObjects(2, handles, true, INFINITE);
+
+	*/
+
+
+
+
+	timer.stop();
+	//
+	// End
+	//
+
+	std::string strTime;
+	strTime = boost::timer::format(timer.elapsed(), ROUNDING_NUMBER, "%u");
 
 	return 0;
 }
